8aedb70c426a5261f181d3800ca91ddb3cb87706
9307006L <nolan.wittmann@reseau.sncf.fr>
2022-08-02 15:15:52 +0200

    Mise à jour

--- src/main/java/org/example/Aquarium.java
+++ src/main/java/org/example/Aquarium.java
@@ -3,22 +3,28 @@ package org.example;
 import java.util.ArrayList;
 
 public class Aquarium {
+
+    private static final String NEMO = "Nemo";
+
     public static void main(String[] args) {
 
-        Fish nemo = new Fish("Nemo");
-        Fish dory = new Fish("Dory");
+        Fish nemo = new Fish(NEMO);
+        String dory1 = "Dory";
+        Fish toto = new Fish(dory1);
         Fish marin = new Fish("Marin");
 
         ArrayList<Fish> leMondeDeNemo = new ArrayList<Fish>();
         leMondeDeNemo.add(nemo);
-        leMondeDeNemo.add(dory);
+        leMondeDeNemo.add(toto);
         leMondeDeNemo.add(marin);
 
-        nemo.setAge(38);
-        dory.setAge(18);
-
+        nemo.setAge(0);
+        toto.setAge(18);
+        leMondeDeNemo.remove(2);
         for (Fish fish : leMondeDeNemo) {
             System.out.println(fish.swimming());
         }
+        leMondeDeNemo.clear();
+
     }
 }
